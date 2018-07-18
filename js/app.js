06c5066d70628a1b260cb3026a07e25e3e20a05b
Princess Gaoaen <pgaoaen@horizon.csueastbay.edu>
2018-07-17 20:57:19 -0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,26 @@
+$(document).foundation()
+
+/*$('.off-canvas a').on('click', function() {
+  $('.off-canvas').foundation('.close-button');
+});*/
+
+$(window).scroll(function(){
+
+  var fromTop = $(window).scrollTop();
+
+  console.log(fromTop);
+
+  if(fromTop >= 0){
+  $("#homeimage").css({
+  "top": fromTop * 0.4 + 0 + "px",
+  })
+  }
+
+  if(fromTop >= 0){
+  $("#homearticle_desktop").css({
+  "top": fromTop * 0.3 + 80 + "px",
+  })
+  }
+
+
+  });
